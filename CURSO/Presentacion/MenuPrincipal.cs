f24ebe5adce047e1293254e10ab4181bb28b7a28
fedejael <federicojael.canto@gmail.com>
2023-05-16 20:18:14 -0300

    creando botones

--- CURSO/Presentacion/MenuPrincipal.cs
+++ CURSO/Presentacion/MenuPrincipal.cs
@@ -7,11 +7,11 @@ using System.Linq;
 using System.Text;
 using System.Windows.Forms;
 
-namespace CURSO
+namespace CURSO.Presentacion
 {
-    public partial class Form1 : Form
+    public partial class MenuPrincipal : Form
     {
-        public Form1()
+        public MenuPrincipal()
         {
             InitializeComponent();
         }
--- CURSO/Program.cs
+++ CURSO/Program.cs
@@ -15,7 +15,7 @@ namespace CURSO
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Presentacion.MenuPrincipal());
         }
     }
 }
