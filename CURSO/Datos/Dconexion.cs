5e667b28b76375c490e4bda9efa1d9f3679eb340
fedejael <federicojael.canto@gmail.com>
2023-05-20 00:22:52 -0300

    Creando cadena de conexion

---
+++ CURSO/Datos/Dconexion.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace CURSO.Datos
+{
+    public class Dconexion
+    {
+        public static string conexion = @"Data source=CABA-FCANTO-NB\SQLEXPRESS; Initial Catalog=CURSO369; Integrated Security=true";
+        public static SqlConnection conectar = new SqlConnection(conexion);
+        public static void abrir()
+        {
+            if(conectar.State ==ConnectionState.Closed)
+            {
+                conectar.Open();
+            }
+        }
+        public static void cerrar()
+        {
+            if (conectar.State == ConnectionState.Open)
+            {
+                conectar.Close();
+            }
+        }
+    }
+}
---
+++ CURSO/Datos/Dpersonal.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+using System.Data.SqlClient;
+using CURSO.Logica;
+
+namespace CURSO.Datos
+{
+    public class Dpersonal
+    {
+        public bool InsertarPersonal(Lpersonal parametros)
+        {
+
+        }
+    }
+}
