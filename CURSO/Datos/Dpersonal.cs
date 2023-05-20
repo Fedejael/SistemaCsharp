b5396e2542f30da187b2ae0b98c0c831ab2716b4
fedejael <federicojael.canto@gmail.com>
2023-05-20 00:53:20 -0300

    Creando los procesos de datos

--- CURSO/Datos/Dpersonal.cs
+++ CURSO/Datos/Dpersonal.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Data;
 using System.Data.SqlClient;
 using CURSO.Logica;
+using System.Windows.Forms;
 
 namespace CURSO.Datos
 {
@@ -12,7 +13,81 @@ namespace CURSO.Datos
     {
         public bool InsertarPersonal(Lpersonal parametros)
         {
+            try
+            {
+                Dconexion.abrir();
+                SqlCommand cmd = new SqlCommand("InsertarPersonal", Dconexion.conectar);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@Nombres", parametros.Nombres);
+                cmd.Parameters.AddWithValue("@Identificacion", parametros.Identificacion);
+                cmd.Parameters.AddWithValue("@Pais", parametros.Pais);
+                cmd.Parameters.AddWithValue("@Id_cargo", parametros.Id_cargo);
+                cmd.Parameters.AddWithValue("@SueldoPorHora", parametros.SueldoPorHora);
+                cmd.ExecuteNonQuery();
+                return true;
 
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message);
+                return false;
+            }
+            finally
+            {
+                Dconexion.cerrar();
+            }
+        }
+        public bool editarPersonal(Lpersonal parametros)
+        {
+            try
+            {
+                Dconexion.abrir();
+                SqlCommand cmd = new SqlCommand("editarPersonal", Dconexion.conectar);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@Id_personal", parametros.Id_personal);
+                cmd.Parameters.AddWithValue("@Nombres", parametros.Nombres);
+                cmd.Parameters.AddWithValue("@Identificacion", parametros.Identificacion);
+                cmd.Parameters.AddWithValue("@Pais", parametros.Pais);
+                cmd.Parameters.AddWithValue("@Id_cargo", parametros.Id_cargo);
+                cmd.Parameters.AddWithValue("@Sueldo_por_hora", parametros.SueldoPorHora);
+                cmd.ExecuteNonQuery();
+                return true;
+
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message);
+                return false;
+            }
+            finally
+            {
+                Dconexion.cerrar();
+            }
+        }
+        public bool eliminarPersonal(Lpersonal parametros)
+        {
+            try
+            {
+                Dconexion.abrir();
+                SqlCommand cmd = new SqlCommand("eliminarPersonal", Dconexion.conectar);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@Idpersonal", parametros.Id_personal);           
+                cmd.ExecuteNonQuery();
+                return true;
+
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message);
+                return false;
+            }
+            finally
+            {
+                Dconexion.cerrar();
+            }
         }
     }
 }
